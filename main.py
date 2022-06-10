554e0466ef3fb80980eb500ca6cbbc4875a7adef
236659 Mikołaj Stępniak <236659@edu.p.lodz.pl>
2022-06-10 15:22:31 +0200

    huge changes to main && reader class

--- main.py
+++ main.py
@@ -1,11 +1,46 @@
-from models.data_managers.reader import load_data
+from models.data_managers.reader import load_data, load_model
+from models.data_managers.writer import save_results, save_model
+from models.neural_network.mlp import MLP
 
 if __name__ == '__main__':
     dn8 = ['f8_1p', 'f8_1z', 'f8_2p', 'f8_2z', 'f8_3p', 'f8_3z']
     dn10 = ['f10_1p', 'f10_1z', 'f10_2p', 'f10_2z', 'f10_3p', 'f10_3z']
 
-    (in_stat8, out_stat8) = load_data(room="F8", col_names=['reference__x', 'reference__y'], file_type="stat")
-    (in_dynamic8, out_dynamic8) = load_data(room="F8", col_names=['reference__x', 'reference__y'], dynamic_names=dn8)
+    choice: int = 0
+    while choice not in [1, 2]:
+        print("[1] - Training mode",
+              "[2] - Testing mode", sep="\n")
+        try:
+            choice = int(input("Choice: "))
+            if choice in [1, 2]:
+                break
+            print("You have to choose 1 or 2 option!")
+        except ValueError:
+            print("Invalid value!")
 
-    (in_stat10, out_stat10) = load_data(room="F10", col_names=['reference__x', 'reference__y'], file_type="stat")
-    (in_dynamic10, out_dynamic10) = load_data(room="F10", col_names=['reference__x', 'reference__y'], dynamic_names=dn10)
+    if choice == 1:
+        (in_stat, out_stat) = load_data(col_names=['data__coordinates__x', 'data__coordinates__y',
+                                                   'reference__x', 'reference__y'], file_type="stat")
+
+        print(in_stat)
+        print(out_stat)
+        model = MLP()
+        model10.train((in_stat, out_stat))
+        print("Saving the learned model...")
+        filename = input("Enter a file name: ")
+        while save_model(model, filename) == "err":
+            filename = input("Model with given file name already exists!")
+
+    if choice == 2:
+        model = None
+        filename = None
+        while model is None:
+            filename = input("Enter a file name of trained model: ")
+            model = load_model(filename)
+
+        (in_dyn, out_dyn) = load_data(col_names=['data__coordinates__x', 'data__coordinates__y',
+                                                 'reference__x', 'reference__y'], dynamic_names=dn8)
+
+        model8 = MLP(model)
+        (mse, weights) = model8.test((in_dyn, out_dyn))
+        save_results(filename, mse, weights)
--- models/data_managers/reader.py
+++ models/data_managers/reader.py
@@ -1,33 +1,45 @@
-import glob
-import os
+from glob import glob
+from os.path import normpath
 import pandas as pd
 from pandas import DataFrame
+from pickle import loads
+from models.neural_network.mlp import MLP
 
 
-def read_excel(file, col_names) -> tuple[DataFrame, DataFrame]:
-    input_data = pd.read_excel(file, sheet_name=0)
-    for i, row in input_data.iterrows():
-        if row['success'] is False:
-            input_data.drop(i)
+def load_model(filename: str) -> MLP or None:
+    try:
+        with open(f"model/{filename}", 'rb') as f:
+            return loads(f.read())
+    except FileNotFoundError:
+        print("File with given name does not exist!")
+        return None
 
-    input_data = input_data[col_names]
-    return input_data, DataFrame(data=input_data)
 
-
-def load_data(room: str, col_names: list, file_type: str = 'dynamic', dynamic_names: list = None) \
+def load_data(col_names: list, file_type: str = 'dynamic', dynamic_names: list = None) \
         -> tuple[DataFrame, DataFrame]:
     _input = DataFrame()
     _output = DataFrame()
 
     if file_type == 'dynamic':
-        files = [f"data/{room}/{d}.xlsx" for d in dynamic_names]
+        files = [f"data/*/{d}.xlsx" for d in dynamic_names]
     else:
-        files = [os.path.normpath(i) for i in glob.glob(f"data/{room}/*{file_type}*.xlsx")]
+        files = [normpath(i) for i in glob(f"data/*/*{file_type}*.xlsx")]
 
     for file in files:
         (in_file, out_file) = read_excel(file, col_names)
         _input = pd.concat([_input, in_file])
         _output = pd.concat([_output, out_file])
 
-    print(f"{file_type.capitalize()} data from room {room} has been successfully loaded.")
+    print(f"{file_type.capitalize()} data has been successfully loaded.")
     return _input, _output
+
+
+def read_excel(file, col_names) -> tuple[DataFrame, DataFrame]:
+    data = pd.read_excel(file, sheet_name=0)
+    to_drop = []
+    for i, row in data.iterrows():
+        if row['success'] is False:
+            to_drop.append(i)
+
+    data = data.drop(data.index[to_drop])
+    return data[col_names[:2]], data[col_names[2:]]
